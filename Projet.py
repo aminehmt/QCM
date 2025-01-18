f217d8b3942450e0f0918a71a5bbaa450939e4e3
Aimene Djebr <151153399+DixTrawZ@users.noreply.github.com>
2025-01-18 15:57:16 +0100

    commit2

--- Projet.py
+++ Projet.py
@@ -39,3 +39,36 @@ def charger_questions(categorie=None): #et had la fct pour charger les questiosn
         print("Erreur : Le fichier questions.json est corrompu.")
         return []
     #si vous avez des qst 9oloLI 
+
+def limite_temps(signum, frame): #timeout ida marepondach f le temps alloué 
+    raise TimeoutError("Temps écoulé pour cette question.")
+
+def passer_test(questions, limite_par_question=30): 
+    signal.signal(signal.SIGALRM, limite_temps)
+    score = 0
+    #print question
+    for i, q in enumerate(questions):
+        print(f"\nQuestion {i + 1}: {q['question']}")
+        for j, option in enumerate(q['options']):
+            print(f"{j + 1}. {option}")
+        try:
+            #timer 
+            signal.alarm(limite_par_question)
+            reponse = int(input("Votre réponse (1-4) : ")) - 1
+            signal.alarm(0) #reponda a temps desactiver timer
+            if reponse == q['reponse']:
+                print("Correct ! ")
+                score += 1
+            else:
+                print(f"Incorrect . La bonne réponse était : {q['options'][q['reponse']]}")
+        except TimeoutError: #depassa le temps 
+            print("\nTemps écoulé pour cette question. Passons à la suivante.")
+            signal.alarm(0)
+    return score
+
+def enregistrer_resultat(users, identifiant, score): #save resultat bch thato f fichier
+    users[identifiant]["historique"].append({
+        "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "score": score
+    })
+    sauvegarder_utilisateurs(users)
